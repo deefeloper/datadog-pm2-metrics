1ca77552de5b8f6b2e24c6d607ba349898ed0255
Dave Derichs <dave@flex-appeal.nl>
2017-03-30 10:13:43 +0200

    implemented event emitter and added service check

--- example-agent.js
+++ example-agent.js
@@ -3,14 +3,17 @@ var DataDogPm2Metrics = require('./lib');
 // Start an agent that automatically sends metrics with a 2.5 second delay between runs
 var dataDogPm2Metrics = new DataDogPm2Metrics({
   autoPulse: true,
-  pulseInterval: 2500,
-  onStart: function () {
-    console.info('Agent started');
-  },
-  onPulse: function () {
-    console.info('Pulse!');
-  },
-  onError: function (err) {
-    console.error('An error has occured: ', err)
-  }
+  pulseInterval: 2500
+});
+
+dataDogPm2Metrics.on('start', function () {
+  console.info('Agent started');
+});
+
+dataDogPm2Metrics.on('pulse', function () {
+  console.info('Pulse!');
+});
+
+dataDogPm2Metrics.on('error', function () {
+  console.error('An error has occured: ', err)
 });
