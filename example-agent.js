cca65f3c84d0cf5b2d2067e3353d712f876fa1c6
Dave Derichs <dave@flex-appeal.nl>
2017-03-31 16:17:31 +0200

    check is more appropriate

--- example-agent.js
+++ example-agent.js
@@ -2,8 +2,11 @@ var DataDogPm2Metrics = require('./lib');
 
 // Start an agent that automatically sends metrics with a 2.5 second delay between runs
 var dataDogPm2Metrics = new DataDogPm2Metrics({
-  autoPulse: true,
-  pulseInterval: 2500
+  autoPulse    : true,
+  pulseInterval: 2500,
+  statsDOptions: {
+    host: 'test.api.flex-appeal.nl'
+  }
 });
 
 dataDogPm2Metrics.on('start', function () {
